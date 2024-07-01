a9c723e8a11a0e12d6b73b06338e4e93c08f6a45
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-07-01 15:07:05 +0700

    Update V2

--- q3.ts
+++ q3.ts
@@ -1,21 +1,20 @@
 function primeNumber(a) {
-  if(a === 1){
-      return "NO"
-     } 
-     for(i = 2;i < a;i++){
-       if(a % i === 0){
-         return "NO"
-         }
-       }
-   return "YES"
-     }
-
-const d1 = 10;
-const d2 = 29;
-const d3 = 2;
-
+    if (a === 1) {
+        return "NO";
+    }
+    for (i = 2; i < a; i++) {
+        if (a % i === 0) {
+            return "NO";
+        }
+    }
+    return "YES";
+}
+var d1 = 10;
+var d2 = 29;
+var d3 = 2;
 console.log(primeNumber(d1));
 console.log(primeNumber(d2));
 console.log(primeNumber(d3));
-
 module.exports = primeNumber;
+
+// นายกรรชัย คุ้มดี 660610739
