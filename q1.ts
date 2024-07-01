c7977b311ac6b2442663ce1a07a34a84a9f5c645
Stranger667 <88589797+StrangerZ667@users.noreply.github.com>
2024-07-01 15:05:36 +0700

    Update V2

--- q1.ts
+++ q1.ts
@@ -1,25 +1,24 @@
 function typeChecker(a, b) {
-  if(typeof a === "string" && typeof b === "string"){
-    return "I LOVE YOU"
-  }
-  else if(typeof a == "number" && typeof b == "number"){
-    return a+b
-  }
-  else{
-    return "NOT MATCHED"
-  }
+    if (typeof a === "string" && typeof b === "string") {
+        return "I LOVE YOU";
+    }
+    else if (typeof a == "number" && typeof b == "number") {
+        return a + b;
+    }
+    else {
+        return "NOT MATCHED";
+    }
 }
-
 //Test cases
-const a1 = "Jack";
-const b1 = "Rose";
-const a2 = 555;
-const b2 = 100;
-const a3 = "Jack";
-const b3 = 100;
-
+var a1 = "Jack";
+var b1 = "Rose";
+var a2 = 555;
+var b2 = 100;
+var a3 = "Jack";
+var b3 = 100;
 console.log(typeChecker(a1, b1));
 console.log(typeChecker(a2, b2));
 console.log(typeChecker(a3, b3));
-
 module.exports = typeChecker;
+
+// นายกรรชัย คุ้มดี 660610739
